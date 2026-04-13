8a2b297938f770ce9cd755bb6fa1b53b5f4fa626
Hanna Devnina <annadevnina@gmail.com>
2026-04-12 22:28:50 -0500

    changed tests names in AccountTest

--- src/test/java/com/bank/model/AccountTest.java
+++ src/test/java/com/bank/model/AccountTest.java
@@ -16,7 +16,7 @@ public class AccountTest {
     }
 
     @Test
-    void shouldThrowIllegalArgumentExceptionWhenCreateAccountWithNegativeBalance() {
+    void shouldThrowWhenBalanceIsNegative() {
         assertThrows(IllegalArgumentException.class,
                 () -> new SavingsAccount("ACC-001", -1000.0,
                         500.0, 0.05, 100.0));
@@ -30,14 +30,14 @@ public class AccountTest {
     }
 
     @Test
-    void shouldThrowIllegalArgumentExceptionWhenCreateAccountWithNegativeTransactionLimit() {
+    void shouldThrowWhenTransactionLimitIsNegative() {
         assertThrows(IllegalArgumentException.class,
                 () -> new SavingsAccount("ACC-001", 0.0,
                         -500.0, 0.05, 100.0));
     }
 
     @Test
-    void shouldThrowIllegalArgumentExceptionWhenCreateAccountWithZeroTransactionLimit() {
+    void shouldThrowWhenTransactionLimitIsZero() {
         assertThrows(IllegalArgumentException.class,
                 () -> new SavingsAccount("ACC-001", 0.0,
                         0.0, 0.05, 100.0));
@@ -50,19 +50,19 @@ public class AccountTest {
     }
 
     @Test
-    void shouldThrowIllegalArgumentExceptionWhenDepositNegative() {
+    void shouldThrowWhenDepositIsNegative() {
         assertThrows(IllegalArgumentException.class,
                 () -> account.deposit("TRX-001", -300));
     }
 
     @Test
-    void shouldThrowIllegalArgumentExceptionWhenDepositZero() {
+    void shouldThrowWhenDepositZero() {
         assertThrows(IllegalArgumentException.class,
                 () -> account.deposit("TRX-001", 0.0));
     }
 
     @Test
-    void shouldThrowAccountClosedExceptionWhenDepositToClosedAccount() {
+    void shouldThrowWhenDepositToClosedAccount() {
         account.setStatus(AccountStatus.CLOSED);
         assertThrows(AccountClosedException.class,
                 () -> account.deposit("TRX-001", 500.0));
