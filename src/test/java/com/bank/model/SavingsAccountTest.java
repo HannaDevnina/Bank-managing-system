3d0ba513a24250be330880c2493f831ef34b86cb
Hanna Devnina <annadevnina@gmail.com>
2026-04-13 21:26:40 -0500

    completed SavingsAccount tests

--- src/test/java/com/bank/model/SavingsAccountTest.java
+++ src/test/java/com/bank/model/SavingsAccountTest.java
@@ -7,6 +7,7 @@ import com.bank.exceptions.TransactionLimitExceededException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SavingsAccountTest {
@@ -61,57 +62,129 @@ class SavingsAccountTest {
 
     @Test
     void shouldDecreaseBalanceWhenWithdrawLessLimit() {
-        account.withdraw("TRX-001", 500);
+        account.withdraw("TRX-000001", 500);
         assertEquals(2000, account.getBalance());
     }
 
     @Test
     void shouldThrowWhenWithdrawAboveLimit() {
         assertThrows(TransactionLimitExceededException.class,
-                () -> account.withdraw("TRX-001", 1500));
+                () -> account.withdraw("TRX-000001", 1500));
     }
 
     @Test
     void shouldNotThrowWhenWithdrawEqualLimit() {
         assertDoesNotThrow(
-                () -> account.withdraw("TRX-001", 1000));
+                () -> account.withdraw("TRX-000001", 1000));
     }
 
     @Test
     void shouldThrowWhenWithdrawZero() {
         assertThrows(IllegalArgumentException.class,
-                () -> account.withdraw("TRX-001", 0));
+                () -> account.withdraw("TRX-000001", 0));
     }
 
     @Test
     void shouldThrowWhenWithdrawFromClosed() {
         account.setStatus(AccountStatus.CLOSED);
         assertThrows(AccountClosedException.class,
-                () -> account.withdraw("TRX-001", 500));
+                () -> account.withdraw("TRX-000001", 500));
     }
 
     @Test
     void shouldThrowWhenWithdrawNegative() {
         assertThrows(IllegalArgumentException.class,
-                () -> account.withdraw("TRX-001", -500));
+                () -> account.withdraw("TRX-000001", -500));
     }
 
     @Test
-    void shouldThrowWhenRemainsBalanceLessMinBalance() {
+    void shouldThrowWhenBalanceWouldGoBelowMinimum() {
         account = new SavingsAccount("ACC-000003", 1000.0, 1000,
                 0.12, 500);
         assertThrows(InsufficientFundsException.class,
-                () -> account.withdraw("TRX-001", 800));
+                () -> account.withdraw("TRX-000001", 800));
     }
 
     @Test
     void shouldCreateTransactionWhenWithdraw() {
-        account.withdraw("TRX-001", 500);
+        account.withdraw("TRX-000001", 500);
         assertEquals(1, account.getTransactions().size());
     }
 
+    @Test
+    void shouldTransferBetweenAccounts() {
+        SavingsAccount destination = new SavingsAccount("ACC-000004", 500.0,
+                1000.0, 0.05, 100.0);
+        account.transfer("TRX-000001", destination, 500);
+        assertEquals(2000, account.getBalance());
+        assertEquals(1000, destination.getBalance());
+    }
 
     @Test
-    void transfer() {
+    void shouldThrowWhenTransferFromClosedAccount() {
+        SavingsAccount destination = new SavingsAccount("ACC-000004", 500.0,
+                1000.0, 0.05, 100.0);
+        account.setStatus(AccountStatus.CLOSED);
+        assertThrows(AccountClosedException.class,
+                () -> account.transfer("TRX-000001", destination, 500 ));
+    }
+
+    @Test
+    void shouldThrowWhenTransferToClosedAccount() {
+        SavingsAccount destination = new SavingsAccount("ACC-000004", 500.0,
+                1000.0, 0.05, 100.0);
+        destination.setStatus(AccountStatus.CLOSED);
+        assertThrows(AccountClosedException.class,
+                () -> account.transfer("TRX-000001", destination, 500));
+    }
+
+    @Test
+    void shouldThrowWhenTransferNegativeAmount() {
+        SavingsAccount destination = new SavingsAccount("ACC-000004", 500.0,
+                1000.0, 0.05, 100.0);
+        assertThrows(IllegalArgumentException.class,
+                () -> account.transfer("TRX-000001", destination, -500));
+    }
+
+    @Test
+    void shouldThrowWhenTransferZero() {
+        SavingsAccount destination = new SavingsAccount("ACC-000004", 500.0,
+                1000.0, 0.05, 100.0);
+        assertThrows(IllegalArgumentException.class,
+                () -> account.transfer("TRX-000001", destination, 0));
+    }
+
+    @Test
+    void shouldThrowWhenTransactionLimitExceeded() {
+        SavingsAccount destination = new SavingsAccount("ACC-000004", 500.0,
+                1000.0, 0.05, 100.0);
+        assertThrows(TransactionLimitExceededException.class,
+                () -> account.transfer("TRX-000001", destination, 1500));
+    }
+
+    @Test
+    void shouldThrowWhenMinBalanceViolated() {
+        account = new SavingsAccount("ACC-000003", 1000.0, 1000,
+                0.12, 500);
+        SavingsAccount destination = new SavingsAccount("ACC-000004", 500.0,
+                1000.0, 0.05, 100.0);
+        assertThrows(InsufficientFundsException.class,
+                () -> account.transfer("TRX-000001", destination, 800));
+    }
+
+    @Test
+    void shouldAddNewTransactionToAccountList() {
+        SavingsAccount destination = new SavingsAccount("ACC-000004", 500.0,
+                1000.0, 0.05, 100.0);
+        account.transfer("TRX-000001", destination, 500);
+        assertEquals(1, account.getTransactions().size());
+    }
+
+    @Test
+    void shouldAddNewTransactionToDestinationAccountList() {
+        SavingsAccount destination = new SavingsAccount("ACC-000004", 500.0,
+                1000.0, 0.05, 100.0);
+        account.transfer("TRX-000001", destination, 500);
+        assertEquals(1, destination.getTransactions().size());
     }
 }
